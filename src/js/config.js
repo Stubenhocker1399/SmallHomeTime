93d20068c9579464d145a8d67b706a3437e517c5
Stubenhocker <Stubenhocker1399@gmail.com>
2017-08-14 19:49:01 +1000

    Initial commit
    Stupid gmt implementation, when for example the gmttime is 23:00 the
    current code would produce an output of 25:00 with an gmt offset of
    02:00.

---
+++ src/js/config.js
@@ -0,0 +1,334 @@
+module.exports = [
+  { 
+    "type": "heading", 
+    "defaultValue": "Small Home Time" 
+  }, 
+  { 
+    "type": "text", 
+    "defaultValue": "Configure the timezones as you desire." 
+  },
+  {
+  "type": "select",
+  "messageKey": "smallGMT",
+  "defaultValue": "disabled",
+  "label": "Small clock time",
+  "options": [
+    { 
+      "label": "UTC-12:00",
+      "value": "utc-1200" 
+    },
+    { 
+      "label": "UTC-11:00",
+      "value": "utc-1100" 
+    },
+    { 
+      "label": "UTC-10:00",
+      "value": "utc-1000" 
+    },
+    { 
+      "label": "UTC-09:30",
+      "value": "utc-0930" 
+    },
+    { 
+      "label": "UTC-09:00",
+      "value": "utc-0900" 
+    },
+    { 
+      "label": "UTC-08:00",
+      "value": "utc-0800" 
+    },
+    { 
+      "label": "UTC-07:00",
+      "value": "utc-0700" 
+    },
+    { 
+      "label": "UTC-06:00",
+      "value": "utc-0600" 
+    },
+    { 
+      "label": "UTC-05:00",
+      "value": "utc-0500" 
+    },
+    { 
+      "label": "UTC-04:00",
+      "value": "utc-0400" 
+    },
+    { 
+      "label": "UTC-03:30",
+      "value": "utc-0330" 
+    },
+    { 
+      "label": "UTC-03:00",
+      "value": "utc-0300" 
+    },
+    { 
+      "label": "UTC-02:00",
+      "value": "utc-0200" 
+    },
+    { 
+      "label": "UTC-01:00",
+      "value": "utc-0100" 
+    },
+    { 
+      "label": "UTC",
+      "value": "utc+0000" 
+    },
+    { 
+      "label": "Disabled",
+      "value": "disabled" 
+    },
+    { 
+      "label": "UTC+01:00",
+      "value": "utc+0100" 
+    },
+    { 
+      "label": "UTC+02:00",
+      "value": "utc+0200" 
+    },
+    { 
+      "label": "UTC+03:00",
+      "value": "utc+0300" 
+    },
+    { 
+      "label": "UTC+04:00",
+      "value": "utc+0400" 
+    },
+    { 
+      "label": "UTC+04:30",
+      "value": "utc+4030" 
+    },
+    { 
+      "label": "UTC+05:00",
+      "value": "utc+0500" 
+    },
+    { 
+      "label": "UTC+05:45",
+      "value": "utc+0545" 
+    },
+    { 
+      "label": "UTC+06:00",
+      "value": "utc+0600" 
+    },
+    { 
+      "label": "UTC+06:30",
+      "value": "utc+0630" 
+    },
+    { 
+      "label": "UTC+07:00",
+      "value": "utc+0700" 
+    },
+    { 
+      "label": "UTC+08:00",
+      "value": "utc+0800" 
+    },
+    { 
+      "label": "UTC+08:30",
+      "value": "utc+0830" 
+    },
+    { 
+      "label": "UTC+08:45",
+      "value": "utc+0845" 
+    },
+    { 
+      "label": "UTC+09:00",
+      "value": "utc+0900" 
+    },
+    { 
+      "label": "UTC+09:45",
+      "value": "utc+0945" 
+    },
+    { 
+      "label": "UTC+10:00",
+      "value": "utc+1000" 
+    },
+    { 
+      "label": "UTC+10:30",
+      "value": "utc+1030" 
+    },
+    { 
+      "label": "UTC+11:00",
+      "value": "utc+1100" 
+    },
+    { 
+      "label": "UTC+12:00",
+      "value": "utc+1200" 
+    },
+    { 
+      "label": "UTC+12:45",
+      "value": "utc+1245" 
+    },
+    { 
+      "label": "UTC+13:00",
+      "value": "utc+1300" 
+    },
+    { 
+      "label": "UTC+14:00",
+      "value": "utc+1400" 
+    }
+  ]
+},
+{
+  "type": "select",
+  "messageKey": "bigGMT",
+  "defaultValue": "localtime",
+  "label": "Big clock time",
+  "options": [
+    { 
+      "label": "UTC-12:00",
+      "value": "utc-1200" 
+    },
+    { 
+      "label": "UTC-11:00",
+      "value": "utc-1100" 
+    },
+    { 
+      "label": "UTC-10:00",
+      "value": "utc-1000" 
+    },
+    { 
+      "label": "UTC-09:30",
+      "value": "utc-0930" 
+    },
+    { 
+      "label": "UTC-09:00",
+      "value": "utc-0900" 
+    },
+    { 
+      "label": "UTC-08:00",
+      "value": "utc-0800" 
+    },
+    { 
+      "label": "UTC-07:00",
+      "value": "utc-0700" 
+    },
+    { 
+      "label": "UTC-06:00",
+      "value": "utc-0600" 
+    },
+    { 
+      "label": "UTC-05:00",
+      "value": "utc-0500" 
+    },
+    { 
+      "label": "UTC-04:00",
+      "value": "utc-0400" 
+    },
+    { 
+      "label": "UTC-03:30",
+      "value": "utc-0330" 
+    },
+    { 
+      "label": "UTC-03:00",
+      "value": "utc-0300" 
+    },
+    { 
+      "label": "UTC-02:00",
+      "value": "utc-0200" 
+    },
+    { 
+      "label": "UTC-01:00",
+      "value": "utc-0100" 
+    },
+    { 
+      "label": "UTC",
+      "value": "utc+0000"
+    },
+    { 
+      "label": "Local Time",
+      "value": "localtime" 
+    },
+    { 
+      "label": "UTC+01:00",
+      "value": "utc+0100" 
+    },
+    { 
+      "label": "UTC+02:00",
+      "value": "utc+0200" 
+    },
+    { 
+      "label": "UTC+03:00",
+      "value": "utc+0300" 
+    },
+    { 
+      "label": "UTC+04:00",
+      "value": "utc+0400" 
+    },
+    { 
+      "label": "UTC+04:30",
+      "value": "utc+4030" 
+    },
+    { 
+      "label": "UTC+05:00",
+      "value": "utc+0500" 
+    },
+    { 
+      "label": "UTC+05:45",
+      "value": "utc+0545" 
+    },
+    { 
+      "label": "UTC+06:00",
+      "value": "utc+0600" 
+    },
+    { 
+      "label": "UTC+06:30",
+      "value": "utc+0630" 
+    },
+    { 
+      "label": "UTC+07:00",
+      "value": "utc+0700" 
+    },
+    { 
+      "label": "UTC+08:00",
+      "value": "utc+0800" 
+    },
+    { 
+      "label": "UTC+08:30",
+      "value": "utc+0830" 
+    },
+    { 
+      "label": "UTC+08:45",
+      "value": "utc+0845" 
+    },
+    { 
+      "label": "UTC+09:00",
+      "value": "utc+0900" 
+    },
+    { 
+      "label": "UTC+09:45",
+      "value": "utc+0945" 
+    },
+    { 
+      "label": "UTC+10:00",
+      "value": "utc+1000" 
+    },
+    { 
+      "label": "UTC+10:30",
+      "value": "utc+1030" 
+    },
+    { 
+      "label": "UTC+11:00",
+      "value": "utc+1100" 
+    },
+    { 
+      "label": "UTC+12:00",
+      "value": "utc+1200" 
+    },
+    { 
+      "label": "UTC+12:45",
+      "value": "utc+1245" 
+    },
+    { 
+      "label": "UTC+13:00",
+      "value": "utc+1300" 
+    },
+    { 
+      "label": "UTC+14:00",
+      "value": "utc+1400" 
+    }
+  ]
+},
+{
+  "type": "submit",
+  "defaultValue": "Save"
+}
+];
